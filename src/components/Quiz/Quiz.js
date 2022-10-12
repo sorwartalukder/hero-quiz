53c512f2192a7d2c87b8d33ab8f0c0af1202668c
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-12 21:37:57 +0600

    add correctAnswer icon

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { EyeIcon } from "@heroicons/react/24/solid";
+import { toast } from 'react-toastify';
 
 const Quiz = ({ quiz }) => {
     console.log(quiz)
     const { question, options, correctAnswer } = quiz;
     return (
-        <div>
-            <h2 >Question: {question.replace(/<[^>]+>/g, '')}</h2>
-            <div>
+        <div className='w-9/12 mx-auto border mt-6 p-3'>
+            <div className='flex justify-between'>
+                <h2 className='text-2xl'>Question: {question.replace(/<[^>]+>/g, '')}</h2>
+                <button>
+                    <EyeIcon className="h-6 w-6 text-blue-500" />
+                </button>
+            </div>
+            <div className='grid grid-cols-2'>
                 {options.map((option, index) => (
                     <div key={index}>
 
-                        <label >
+                        <label className='flex mt-2 '>
                             <input
                                 type="radio"
                                 name="radio"
@@ -18,9 +25,8 @@ const Quiz = ({ quiz }) => {
                                 className="radio checked:bg-red-500"
                             />
 
-                            <span>{option}</span>
+                            <span className='ml-2'>{option}</span>
                         </label>
-
 
                     </div>
                 ))}
