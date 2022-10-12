426a379664019f681b21e5ce29ab8f94ff6db7f6
sorwartalukder <mdsorwar4039@gmail.com>
2022-10-12 18:31:41 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Topics from './components/Topics/Topics';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        { path: '/', element: <Topics></Topics> },
+        { path: '/blog', element: <Blog></Blog> }
+      ]
+    }
+  ])
   return (
     <div>
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='bg-violet-300 '>
+            <div className=' flex justify-between items-center py-4 w-10/12 mx-auto'>
+                <h1 className='text-3xl'>Hero Quaz</h1>
+                <div >
+                    <Link className='mr-4 text-xl' to="/">Home</Link>
+                    <Link className='mr-4 text-xl' to="/">Topics</Link>
+                    <Link className='mr-4 text-xl' to="/blog">Blog</Link>
+                    <Link className='mr-4 text-xl' to="/blog">About</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
